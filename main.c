245fc46a774e9dc8efd76554b227b78259a7802e
OverLord <overlord7wizard@gmail.com>
2017-11-29 00:35:54 +0200

    04

--- main.c
+++ main.c
@@ -2,10 +2,25 @@
 #include <stdlib.h>
 void test()
 {
-    gets();
-    printf(error);
-    scanf();
+ana
+agmd
+wa7d
+fel 3alm
+#rgola_2018
+34an ana mwl3ha
+bysmony el nigger
+34an ana 4rs
+w ga7d
+w sfa7
+bs el dod howa ele bygyb 4 gpa
+fa ana 7zyn
+w hany 7zyn
+bs el dod 7zyn
+34an n2s drga
+w tl3 m2fl
+fa howa 3l2
 }
+
 void main()
 {
 printf("m4 znby :')");
